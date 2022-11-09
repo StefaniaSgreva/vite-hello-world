debb4e24082cd032993647306e7b7884d030f998
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-09 16:44:10 +0100

    added AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <header>
+        <h1>{{titol}}</h1>
+    </header>
+
+</template>
+  
+<script>
+    export default {
+        name: 'AppTitle',
+        data(){
+            return{
+            titol: 'La mia prima app con Vite!'
+            }
+        },
+    }
+</script>
+
+<style lang="css">
+header{
+    width: 100%;
+    height: 90px;
+    background-color: bisque;
+    padding: 20px;
+}
+h1 {
+    color: green;
+    text-align: center;
+    font-size: 38px;
+}
+</style>
